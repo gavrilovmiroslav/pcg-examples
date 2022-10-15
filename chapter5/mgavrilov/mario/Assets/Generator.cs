5ae537e8088d97d82bbf169e6ff07404354ec7c1
Miroslav <gavrilov.miroslav@gmail.com>
2022-10-15 16:10:50 +0200

    Made mario shiny

--- chapter5/mgavrilov/mario/Assets/Generator.cs
+++ chapter5/mgavrilov/mario/Assets/Generator.cs
@@ -5,22 +5,32 @@ using Sirenix.OdinInspector;
 
 public class Generator : MonoBehaviour
 {
+    [Header("Chunk Prefabs")]
     public GameObject PlatformPrefab;
-    
+
+    [Space]
     public GameObject HillPrefab;
     public GameObject HillTopPrefab;
 
+    [Space]
     public GameObject BillPrefab;
     public GameObject BillTopPrefab;
 
+    [Space]
     public GameObject PipePrefab;
     public GameObject PipeTopPrefab;
 
+    [Space]
     public GameObject CoinPrefab;
+    
+    [Space]
     public GameObject BoxPrefab;
 
+    [Header("Genotype Properties")]
     public int Seed = 42;
     public int GenotypeLength = 10;
+
+    [Space]
     public int GenotypeLow = 0;
     public int GenotypeHigh = 100;
 
@@ -43,8 +53,38 @@ public class Generator : MonoBehaviour
         Generate();
     }
 
+    private string _seed;
+    public void OnGUI()
+    {        
+        GUILayout.Space(20);
+        GUILayout.BeginHorizontal();
+        GUILayout.Label($"  Seed: ");
+
+        _seed = GUILayout.TextField($"{_seed}", GUILayout.ExpandWidth(false), GUILayout.Width(50));
+        if (GUILayout.Button("Set"))
+        {
+            if (int.TryParse(_seed, out int res))
+            {
+                Seed = res;
+                Generate();
+            }
+        }
+
+        if (GUILayout.Button("Prev"))
+        {
+            PrevLevel();
+        }
+        
+        if (GUILayout.Button("Next"))
+        {
+            NextLevel();
+        }
+        GUILayout.EndHorizontal();        
+    }
+
     public void Generate()
     {
+        _seed = $"{Seed}";
         Random.InitState(Seed);
 
         Queue<int> gen = new();
@@ -107,10 +147,12 @@ public class Generator : MonoBehaviour
             {
                 if (level[i, j, (int)EChunkType.TubeHill] > 0)
                 {
+                    if (j > 0 && level[i, j - 1, (int)EChunkType.BlasterBill] > 0) continue;
                     Instantiate(level[i, j, (int)EChunkType.TubeHill] == 2 ? PipeTopPrefab : PipePrefab, new Vector3(i, j), Quaternion.identity);
                 }
                 else if (level[i, j, (int)EChunkType.BlasterBill] > 0)
                 {
+                    if (j > 0 && level[i, j - 1, (int)EChunkType.TubeHill] > 0) continue;
                     Instantiate(level[i, j, (int)EChunkType.BlasterBill] == 2 ? BillTopPrefab : BillPrefab, new Vector3(i, j), Quaternion.identity);
                 }                
                 else if (level[i, j, (int)EChunkType.Hill] > 0)
